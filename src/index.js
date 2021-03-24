9b05f965347accc62a3ffe0eb4978c97a4ac2cde
zodea <zd929560638@gmail.com>
2021-03-24 22:15:40 +0800

    commit 2.4

--- src/index.js
+++ src/index.js
@@ -2,34 +2,52 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4,
-      },
-    ],
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ]
 
-  return <Course course={course} />
+  return courses.map((course, index) => <Course key={index} course={course} />)
 }
 const Course = ({ course }) => {
   return (
@@ -64,7 +82,7 @@ const Part = ({ part: { name, exercises } }) => {
 }
 const Total = ({ course: { parts } }) => {
   const total = parts.reduce((prev, next) => prev + next.exercises, 0)
-  return <div>all: {total}</div>
+  return <div>total of {total} exercises</div>
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
