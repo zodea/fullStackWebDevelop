caf6c0709cdbd5546eb76bfe63ae2ddc12fabf94
zodea <zd929560638@gmail.com>
2021-03-24 21:24:24 +0800

    commit 2.1

--- src/index.js
+++ src/index.js
@@ -1,25 +1,67 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App.js'
 
-const notes = [
-  {
+const App = () => {
+  const course = {
     id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false,
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true,
-  },
-]
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  }
 
-ReactDOM.render(<App notes={notes} />, document.getElementById('root'))
+  return <Course course={course} />
+}
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+const Header = ({ course: { name } }) => {
+  return <h1>{name}</h1>
+}
+const Content = ({ course: { parts } }) => {
+  return (
+    <div>
+      <Part part={parts[0]}></Part>
+      <Part part={parts[1]}></Part>
+      <Part part={parts[2]}></Part>
+    </div>
+  )
+}
+
+const Part = ({ part: { name, exercises } }) => {
+  return (
+    <>
+      <div>{name} {exercises}</div>
+    </>
+  )
+}
+const Total = props => {
+  let total = 0
+  props.course.parts.map(res => {
+    total += res.exercises
+    return res
+  })
+  return <div>all: {total}</div>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
