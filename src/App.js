48b1b12e089eaa35fd0ad955ecf71fbe4edc7329
zodea <zd929560638@gmail.com>
2021-03-29 17:11:48 +0800

    Promise and Error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -9,12 +9,21 @@ const App = () => {
 
   useEffect(() => {
     console.log('effect')
-    axios.get('http://localhost:3001/notes').then(response => {
-      console.log('promise fulfilled')
-      setNotes(response.data)
+    noteService.getAll().then(initialNotes => {
+      setNotes(initialNotes)
     })
   }, [])
-  console.log('render', notes.length, 'notes')
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changeNote = { ...note, important: !note.important }
+    noteService.update(id, changeNote).then(returnedNote => {
+      setNotes(notes.map(note => (note.id !== id ? note : returnedNote)))
+    }).catch(error => {
+      alert(`the note '${note.content}' was already deleted from server`)
+      setNotes(notes.filter(n => n.id !== id))
+    })
+  }
 
   const addNote = event => {
     event.preventDefault()
@@ -24,8 +33,8 @@ const App = () => {
       important: Math.random() > 0.5,
     }
 
-    axios.post('http://localhost:3001/notes', noteObject).then(response => {
-      setNotes(notes.concat(response.data))
+    noteService.create(noteObject).then(returnedNote => {
+      setNotes(notes.concat(returnedNote))
       setNewNote('')
     })
   }
@@ -33,16 +42,6 @@ const App = () => {
   const handleNoteChange = event => {
     console.log(event.target.value)
     setNewNote(event.target.value)
-  }
-
-  const toggleImportanceOf = id => {
-    const url = `http://localhost:3001/notes/${id}`
-    const note = notes.find(n => n.id === id)
-    const changeNote = { ...note, important: !note.important }
-
-    axios.put(url, changeNote).then(response => {
-      setNotes(notes.map(note => (note.id !== id ? note : response.data)))
-    })
   }
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
