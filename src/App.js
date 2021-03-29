b0972c5d4d0df88e9bcb7047f3b07fe6e0ab3e07
zodea <zd929560638@gmail.com>
2021-03-29 19:36:20 +0800

    2-17

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import phone from './services/phonebook'
 
 const App = () => {
   const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
+  const [number, setNumber] = useState('')
+  const [name, setName] = useState('')
+  const [filterPersons, setFilterPersons] = useState([])
 
   useEffect(() => {
     phone.getAll().then(res => {
@@ -13,39 +15,63 @@ const App = () => {
 
   const addToBook = event => {
     event.preventDefault()
-    const repeatName = persons.filter(person => person.name === newName)
+    const repeatName = persons.filter(person => person.name === name)
     if (repeatName && repeatName.length) {
-      alert(`${newName} is already added to phonebook`)
+      alert(`${name} is already added to phonebook`)
       return
     }
-    phone.create({ name: newName }).then(res => {
+    phone.create({ name, number }).then(res => {
       const newPerson = persons.concat(res)
       setPersons(newPerson)
-      setNewName('')
+      setName('')
+      setNumber('')
     })
   }
 
-  const changeName = event => {
-    setNewName(event.target.value)
+  const deletePerson = person => {
+    if (window.confirm(`Delete ${person.name} ?`)) {
+      phone.remove(person.id).then(status => {
+        if (status === 200) {
+          setPersons(persons.filter(data => data.id !== person.id))
+        }
+      })
+    }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <Input value={filterPersons} onChange={setFilterPersons} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addToBook}>
         <div>
-          name: <input value={newName} onChange={changeName} />
+          name <Input value={name} onChange={setName} />
+        </div>
+        <div>
+          number <Input value={number} onChange={setNumber} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, index) => (
-        <div key={index}>{person.name}</div>
+      {persons.map(person => (
+        <div key={person.id}>
+          {person.name} {person.number}
+          <button onClick={() => deletePerson(person)}>delete</button>
+        </div>
       ))}
     </div>
   )
+}
+
+const Input = ({ value, onChange }) => {
+  const changeHandle = event => {
+    onChange(event.target.value)
+  }
+  return <input value={value} onChange={changeHandle}></input>
 }
 
 export default App
--- src/services/phonebook.js
+++ src/services/phonebook.js
@@ -10,5 +10,9 @@ const create = content => {
   const response = axios.post(baseUrl, content)
   return response.then(res => res.data)
 }
+const remove = id => {
+  const response = axios.delete(`${baseUrl}/${id}`)
+  return response.then(res => res.status)
+}
 
-export default { getAll, create }
+export default { getAll, create, remove }
