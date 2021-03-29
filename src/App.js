c0b14b7a05d44900c3d11d6112b594f76c163b74
zodea <zd929560638@gmail.com>
2021-03-29 11:12:20 +0800

    changing the importance of note

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import Note from './components/Note'
 import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import Note from './components/Note'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -35,6 +35,16 @@ const App = () => {
     setNewNote(event.target.value)
   }
 
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changeNote = { ...note, important: !note.important }
+
+    axios.put(url, changeNote).then(response => {
+      setNotes(notes.map(note => (note.id !== id ? note : response.data)))
+    })
+  }
+
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
   return (
@@ -45,7 +55,7 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note => (
-          <Note key={note.id} note={note} />
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
         ))}
       </ul>
       <form onSubmit={addNote}>
